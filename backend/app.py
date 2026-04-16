2ad918e21a50c70307cb5ef15b422885ff2f38c5
palakchaudhar-y <palakchaudhary367@gmail.com>
2026-04-17 00:41:38 +0530

    Updated app.py

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,11 @@ from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import os
 
 app = Flask(__name__)
 
+# load model
 model = tf.keras.models.load_model("waste_model.h5")
 
 IMG_SIZE = 224
@@ -14,6 +16,11 @@ def preprocess_image(image):
     image = np.array(image) / 255.0
     image = np.expand_dims(image, axis=0)
     return image
+
+
+@app.route("/")
+def home():
+    return "EcoSort AI backend is running!"
 
 
 @app.route("/predict", methods=["POST"])
@@ -31,13 +38,11 @@ def predict():
 
     prob = float(prediction[0][0])
 
-    # convert probability to confidence
     confidence = max(prob, 1 - prob)
 
     print("Raw probability:", prob)
     print("Confidence:", confidence)
 
-    # 🔥 confidence check
     if confidence >= 0.70:
 
         if prob > 0.5:
@@ -57,4 +62,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
